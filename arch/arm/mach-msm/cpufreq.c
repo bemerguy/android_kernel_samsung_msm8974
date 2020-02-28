c2d79c2b1c47790f3721dbb7d3103b2616e1290a
Heiler Bemerguy <heiler.bemerguy@gmail.com>
2020-02-28 19:58:28 -0300

    Timer back to 32bit and cpufreq reverts

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -445,22 +445,25 @@ static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 	unsigned int cpu = (unsigned long)hcpu;
 	int rc;
 
-	/* Fail hotplug until this driver can get CPU clocks */
-	if (!hotplug_ready)
-		return NOTIFY_BAD;
-
 	switch (action & ~CPU_TASKS_FROZEN) {
-	case CPU_DYING:
-		clk_disable(cpu_clk[cpu]);
-		clk_disable(l2_clk);
+	case CPU_ONLINE:
+		per_cpu(cpufreq_suspend, cpu).device_suspended = 0;
+		break;
+	case CPU_DOWN_PREPARE:
+		mutex_lock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
+		per_cpu(cpufreq_suspend, cpu).device_suspended = 1;
+		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
+		break;
+	case CPU_DOWN_FAILED:
+		per_cpu(cpufreq_suspend, cpu).device_suspended = 0;
 		break;
 	/*
 	 * Scale down clock/power of CPU that is dead and scale it back up
 	 * before the CPU is brought up.
 	 */
 	case CPU_DEAD:
-		clk_unprepare(cpu_clk[cpu]);
-		clk_unprepare(l2_clk);
+		clk_disable_unprepare(cpu_clk[cpu]);
+		clk_disable_unprepare(l2_clk);
 		update_l2_bw(NULL);
 		break;
 	case CPU_UP_CANCELED:
@@ -473,10 +476,8 @@ static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		if (rc < 0)
 			return NOTIFY_BAD;
 		rc = clk_prepare(cpu_clk[cpu]);
-		if (rc < 0) {
-			clk_unprepare(l2_clk);
+		if (rc < 0)
 			return NOTIFY_BAD;
-		}
 		update_l2_bw(&cpu);
 		break;
 	case CPU_STARTING:
@@ -484,10 +485,8 @@ static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		if (rc < 0)
 			return NOTIFY_BAD;
 		rc = clk_enable(cpu_clk[cpu]);
-		if (rc) {
-			clk_disable(l2_clk);
+                if (rc < 0)
 			return NOTIFY_BAD;
-		}
 		break;
 	default:
 		break;
@@ -500,71 +499,36 @@ static struct notifier_block __refdata msm_cpufreq_cpu_notifier = {
 	.notifier_call = msm_cpufreq_cpu_callback,
 };
 
-static int msm_cpufreq_suspend(void)
+/*
+ * Define suspend/resume for cpufreq_driver. Kernel will call
+ * these during suspend/resume with interrupts disabled. This
+ * helps the suspend/resume variable get's updated before cpufreq
+ * governor tries to change the frequency after coming out of suspend.
+ */
+static int msm_cpufreq_suspend(struct cpufreq_policy *policy)
+{
+ 	int cpu;
+
+ 	for_each_possible_cpu(cpu) {
+ 		per_cpu(cpufreq_suspend, cpu).device_suspended = 1;
+ 	}
+
+	return 0;
+}
+
+static int msm_cpufreq_resume(struct cpufreq_policy *policy)
 {
 	int cpu;
 
-	for_each_possible_cpu(cpu) {
-		mutex_lock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
-		per_cpu(cpufreq_suspend, cpu).device_suspended = 1;
-		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
-	}
-
-	return NOTIFY_DONE;
-}
-
-static int msm_cpufreq_resume(void)
-{
-	int cpu, ret;
-	struct cpufreq_policy policy;
+	printk("msm_cpufreq_resume\n");
 
 	for_each_possible_cpu(cpu) {
 		per_cpu(cpufreq_suspend, cpu).device_suspended = 0;
 	}
 
-	/*
-	 * Freq request might be rejected during suspend, resulting
-	 * in policy->cur violating min/max constraint.
-	 * Correct the frequency as soon as possible.
-	 */
-	get_online_cpus();
-	for_each_online_cpu(cpu) {
-		ret = cpufreq_get_policy(&policy, cpu);
-		if (ret)
-			continue;
-		if (policy.cur <= policy.max && policy.cur >= policy.min)
-			continue;
-		ret = cpufreq_update_policy(cpu);
-		if (ret)
-			pr_err("cpufreq: Current frequency violates policy min/max for CPU%d\n",
-			       cpu);
-		else
-			pr_debug("cpufreq: Frequency violation fixed for CPU%d\n",
-				cpu);
-	}
-	put_online_cpus();
+	return 0;
 
-	return NOTIFY_DONE;
 }
-
-static int msm_cpufreq_pm_event(struct notifier_block *this,
-				unsigned long event, void *ptr)
-{
-	switch (event) {
-	case PM_POST_HIBERNATION:
-	case PM_POST_SUSPEND:
-		return msm_cpufreq_resume();
-	case PM_HIBERNATION_PREPARE:
-	case PM_SUSPEND_PREPARE:
-		return msm_cpufreq_suspend();
-	default:
-		return NOTIFY_DONE;
-	}
-}
-
-static struct notifier_block msm_cpufreq_pm_notifier = {
-	.notifier_call = msm_cpufreq_pm_event,
-};
 
 static struct freq_attr *msm_freq_attr[] = {
 	&cpufreq_freq_attr_scaling_available_freqs,
@@ -578,6 +542,8 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 	.verify		= msm_cpufreq_verify,
 	.target		= msm_cpufreq_target,
 	.get		= msm_cpufreq_get_freq,
+	.suspend	= msm_cpufreq_suspend,
+	.resume		= msm_cpufreq_resume,
 	.name		= "msm",
 	.attr		= msm_freq_attr,
 };
@@ -651,12 +617,11 @@ static int cpufreq_parse_dt(struct device *dev)
 
 		freq_table[i].index = i;
 		freq_table[i].frequency = f;
-		printk("Tuned adding freq_table %d (%d)\n", f, i);
 
 		if (l2_clk) {
 			f = clk_round_rate(l2_clk, data[j++] * 1000);
 			if (IS_ERR_VALUE(f)) {
-				printk("Tuned Error finding L2 rate for CPU %d KHz\n",
+				printk("Error finding L2 rate for CPU %d KHz\n",
 					freq_table[i].frequency);
 				freq_table[i].frequency = CPUFREQ_ENTRY_INVALID;
 			} else {
@@ -679,10 +644,8 @@ static int cpufreq_parse_dt(struct device *dev)
 	if (!dts_freq_table)
 		return -ENOMEM;
 
-	for (i = 0, j = 0; i < nf; i++, j += 3) {
+	for (i = 0, j = 0; i < nf; i++, j += 3)
 		dts_freq_table[i].frequency = data[j];
-		printk("Tuned adding dts_freq_table %d\n", data[j]);
-	}
 	dts_freq_table[i].frequency = CPUFREQ_TABLE_END;
 #endif
 
@@ -824,15 +787,9 @@ static int __init msm_cpufreq_register(void)
 	}
 
 	msm_cpufreq_wq = alloc_workqueue("msm-cpufreq", WQ_HIGHPRI, 0);
-	register_pm_notifier(&msm_cpufreq_pm_notifier);
+	register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
 
 	return cpufreq_register_driver(&msm_cpufreq_driver);
 }
 
 device_initcall(msm_cpufreq_register);
-
-static int __init msm_cpufreq_early_register(void)
-{
-	return register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
-}
-core_initcall(msm_cpufreq_early_register);
