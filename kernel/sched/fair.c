45691baf4c9c53b1cdb6eb204169ac09a4c9d9e0
Heiler Bemerguy <heiler.bemerguy@gmail.com>
2020-05-21 12:58:45 -0300

    Fair scheduler with hard-coded LOS values

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -43,8 +43,8 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 8000000ULL;
-unsigned int normalized_sysctl_sched_latency = 8000000ULL;
+unsigned int sysctl_sched_latency = 10000000ULL;
+unsigned int normalized_sysctl_sched_latency = 10000000ULL;
 
 /*
  * The initial- and re-scaling of tunables is configurable
@@ -62,13 +62,13 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
  * Minimal preemption granularity for CPU-bound tasks:
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 2000000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 2000000ULL;
+unsigned int sysctl_sched_min_granularity = 2250000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 2250000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 4;
+static unsigned int sched_nr_latency = 8;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -95,7 +95,7 @@ unsigned int __read_mostly sysctl_sched_wake_to_idle;
 unsigned int sysctl_sched_wakeup_granularity = 2000000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity = 2000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost = 2500000UL;
+const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
